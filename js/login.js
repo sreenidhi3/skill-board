a945f6e849a0aaca6d7218cd36ab7a75d2655d56
Sreenidhi Bendre <67953001+sreenidhi3@users.noreply.github.com>
2020-10-04 00:29:49 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -14,7 +14,9 @@ async function postData(url , data) {
       body: JSON.stringify(data),
     });
     if(response.status == "200"){
-      swal("Good job!", "Login Succesful!!", "success").then(function(){window.location.href = "index.html";});
+      swal("Good job!", "Login Succesful!!", "success").then(function(){
+          window.location.href = "index.html";
+      });
       
     } else{
       alert("Something went wrong. Try Again!")
